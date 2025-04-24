884d7d5d180bf008af018f0264780b4bde78f0de
Eduardo Henrique <dudu30rafa@gmail.com>
2025-04-24 12:19:38 -0300

    Update country flags

---
+++ script/flags.js
@@ -0,0 +1,28 @@
+const countryFlags = {
+    USD: 'US',
+    EUR: 'EU',
+    BRL: 'BR',
+    GBP: 'GB',
+    JPY: 'JP',
+    AUD: 'AU',
+    CAD: 'CA',
+    CHF: 'CH',
+    CNY: 'CN',
+    ARS: 'AR',
+};
+
+const fromCurrency = document.getElementById('from-currency');
+const toCurrency = document.getElementById('to-currency');
+
+const fromFlag = fromCurrency.closest('.select-box').querySelector('img');
+const toFlag = toCurrency.closest('.select-box').querySelector('img');
+
+fromCurrency.addEventListener('change', function() {
+    const selectedCurrency = fromCurrency.value;
+    fromFlag.src = `https://flagcdn.com/w20/${countryFlags[selectedCurrency].toLowerCase()}.png`;
+});
+
+toCurrency.addEventListener('change', function() {
+    const selectedCurrency = toCurrency.value;
+    toFlag.src = `https://flagcdn.com/w20/${countryFlags[selectedCurrency].toLowerCase()}.png`;
+});
